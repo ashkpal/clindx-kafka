6bdfa90691548a64700bbad91bd3cb67d912586b
Ashish Kheterpal <ashkpal@yahoo.com>
2026-03-11 15:35:46 -0700

    TestOrderUpdate

---
+++ models/test_order_update_event.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TestOrderUpdateEvent struct {
+	EventVersion string    `json:"event_version"`
+	EventType    string    `json:"event_type"`
+	OccurredAt   time.Time `json:"occurred_at"`
+
+	TestOrderID uint `json:"next_portal_order_id"`
+	//LIMSAccessionID          string    `json:"accession_id"`
+	LIMSAccessionTestOrderID uuid.UUID `json:"portal_test_order_id"`
+	LIMSTestRequisitionID    uuid.UUID `json:"test_order_form_id"`
+	Status                   string    `json:"current_status"`
+	Diagnosis                string    `json:"diagnosis"`
+	DiagnosisOther           string    `json:"diagnosis_other"`
+}
