4af3611edab2967295116ed84d4b90b021cec900
Ashish Kheterpal <ashkpal@yahoo.com>
2026-03-11 18:53:16 -0700

    Order Consumer

---
+++ consumer/order_consumer.go
@@ -0,0 +1,84 @@
+package consumer
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/ashkpal/clindx-kafka/models"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/scram"
+)
+
+// TestOrderUpdate is implemented by the application
+type OrderHandler func(ctx context.Context, event *models.TestOrderUpdateEvent) error
+
+func ConsumeTestOrderUpdates(
+	ctx context.Context,
+	cfg Config,
+	handler OrderHandler,
+) error {
+
+	mechanism, err := scram.Mechanism(
+		scram.SHA512,
+		cfg.Username,
+		cfg.Password,
+	)
+	if err != nil {
+		return err
+	}
+
+	dialer := &kafka.Dialer{
+		Timeout:       30 * time.Second,
+		DualStack:     true,
+		SASLMechanism: mechanism,
+		TLS:           &tls.Config{},
+	}
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: cfg.Brokers,
+		Topic:   cfg.Topic,
+		GroupID: cfg.GroupID,
+		Dialer:  dialer,
+	})
+	defer reader.Close()
+
+	log.Printf("[Kafka] Consuming test order updates from topic=%s", cfg.Topic)
+
+	backoff := time.Second
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("[Kafka] TestOrder consumer shutting down")
+			return nil
+
+		default:
+			msg, err := reader.ReadMessage(ctx)
+			if err != nil {
+				if ctx.Err() != nil {
+					return nil
+				}
+				log.Printf("[Kafka] read error: %v", err)
+				time.Sleep(backoff)
+				backoff = min(backoff*2, time.Minute)
+				continue
+			}
+			backoff = time.Second
+
+			var event models.TestOrderUpdateEvent
+			if err := json.Unmarshal(msg.Value, &event); err != nil {
+				log.Printf("[Kafka] invalid JSON: %v", err)
+				continue
+			}
+
+			if err := handler(ctx, &event); err != nil {
+				log.Printf("[Kafka] handler error (key=%s): %v", string(msg.Key), err)
+				// at-least-once semantics: do NOT crash
+				continue
+			}
+		}
+	}
+}
