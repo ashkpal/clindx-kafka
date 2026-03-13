66b78464c103a1a2caca4662f185c7bcd11907ff
Ashish Kheterpal <ashkpal@yahoo.com>
2026-03-13 14:46:24 -0700

    collection dates

--- models/test_order_update_event.go
+++ models/test_order_update_event.go
@@ -11,12 +11,14 @@ type TestOrderUpdateEvent struct {
 	EventType    string    `json:"event_type"`
 	OccurredAt   time.Time `json:"occurred_at"`
 
-	TestOrderID              uint      `json:"next_portal_order_id"`
-	PatientID                uint      `json:"next_portal_patient_id"`
-	LIMSAccessionID          string    `json:"accession_id"`
-	LIMSAccessionTestOrderID uuid.UUID `json:"portal_test_order_id"`
-	LIMSTestRequisitionID    uuid.UUID `json:"test_order_form_id"`
-	Status                   string    `json:"current_status"`
-	Diagnosis                string    `json:"diagnosis"`
-	DiagnosisOther           string    `json:"diagnosis_other"`
+	TestOrderID              uint       `json:"next_portal_order_id"`
+	PatientID                uint       `json:"next_portal_patient_id"`
+	LIMSAccessionID          string     `json:"accession_id"`
+	LIMSAccessionTestOrderID uuid.UUID  `json:"portal_test_order_id"`
+	LIMSTestRequisitionID    uuid.UUID  `json:"test_order_form_id"`
+	Status                   string     `json:"current_status"`
+	Diagnosis                string     `json:"diagnosis"`
+	DiagnosisOther           string     `json:"diagnosis_other"`
+	BloodCollectionDate      *time.Time `json:"plasma_collection_date"`
+	TumorCollectionDate      *time.Time `json:"tumor_collection_date"`
 }
