fd80a0ecdb300e1e86ffbe9caec2a8d7b844bb65
Ashish Kheterpal <ashkpal@yahoo.com>
2026-04-24 05:47:17 -0700

    paperTRFHandlers

--- consumer/order_consumer.go
+++ consumer/order_consumer.go
@@ -12,6 +12,156 @@ import (
 	"github.com/segmentio/kafka-go/sasl/scram"
 )
 
+// PatientHandler is implemented by the application
+type PatientHandler func(ctx context.Context, event *models.PatientEvent) error
+
+func ConsumePatient(
+	ctx context.Context,
+	cfg Config,
+	handler PatientHandler,
+) error {
+
+	mechanism, err := scram.Mechanism(
+		scram.SHA512,
+		cfg.Username,
+		cfg.Password,
+	)
+	if err != nil {
+		return err
+	}
+
+	dialer := &kafka.Dialer{
+		Timeout:       30 * time.Second,
+		DualStack:     true,
+		SASLMechanism: mechanism,
+		TLS:           &tls.Config{},
+	}
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: cfg.Brokers,
+		Topic:   cfg.Topic,
+		GroupID: cfg.GroupID,
+		Dialer:  dialer,
+	})
+	defer reader.Close()
+
+	log.Printf("[Kafka] Consuming patient from topic=%s", cfg.Topic)
+
+	backoff := time.Second
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("[Kafka] patient consumer shutting down")
+			return nil
+
+		default:
+			msg, err := reader.ReadMessage(ctx)
+
+			if err != nil {
+				log.Printf("[Kafka] patient read error: %+v", err)
+				log.Printf("username=%s", cfg.Username)
+				if ctx.Err() != nil {
+					return nil
+				}
+				log.Printf("[Kafka] patient read error: %v", err)
+				time.Sleep(backoff)
+				backoff = min(backoff*2, time.Minute)
+				continue
+			}
+			backoff = time.Second
+
+			log.Printf("Raw message: %s", string(msg.Value))
+			var event models.PatientEvent
+			if err := json.Unmarshal(msg.Value, &event); err != nil {
+				log.Printf("[Kafka] invalid JSON: %v", err)
+				continue
+			}
+
+			if err := handler(ctx, &event); err != nil {
+				log.Printf("[Kafka] handler error (key=%s): %v", string(msg.Key), err)
+				// at-least-once semantics: do NOT crash
+				continue
+			}
+		}
+	}
+}
+
+// PaperTRFHandler is implemented by the application
+type PaperTRFHandler func(ctx context.Context, event *models.TestOrderEvent) error
+
+func ConsumePaperTRF(
+	ctx context.Context,
+	cfg Config,
+	handler PaperTRFHandler,
+) error {
+
+	mechanism, err := scram.Mechanism(
+		scram.SHA512,
+		cfg.Username,
+		cfg.Password,
+	)
+	if err != nil {
+		return err
+	}
+
+	dialer := &kafka.Dialer{
+		Timeout:       30 * time.Second,
+		DualStack:     true,
+		SASLMechanism: mechanism,
+		TLS:           &tls.Config{},
+	}
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: cfg.Brokers,
+		Topic:   cfg.Topic,
+		GroupID: cfg.GroupID,
+		Dialer:  dialer,
+	})
+	defer reader.Close()
+
+	log.Printf("[Kafka] Consuming paper TRF from topic=%s", cfg.Topic)
+
+	backoff := time.Second
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("[Kafka] Paper TRF consumer shutting down")
+			return nil
+
+		default:
+			msg, err := reader.ReadMessage(ctx)
+
+			if err != nil {
+				log.Printf("[Kafka] Paper TRF read error: %+v", err)
+				log.Printf("username=%s", cfg.Username)
+				if ctx.Err() != nil {
+					return nil
+				}
+				log.Printf("[Kafka] Paper TRF read error: %v", err)
+				time.Sleep(backoff)
+				backoff = min(backoff*2, time.Minute)
+				continue
+			}
+			backoff = time.Second
+
+			log.Printf("Raw message: %s", string(msg.Value))
+			var event models.TestOrderEvent
+			if err := json.Unmarshal(msg.Value, &event); err != nil {
+				log.Printf("[Kafka] invalid JSON: %v", err)
+				continue
+			}
+
+			if err := handler(ctx, &event); err != nil {
+				log.Printf("[Kafka] handler error (key=%s): %v", string(msg.Key), err)
+				// at-least-once semantics: do NOT crash
+				continue
+			}
+		}
+	}
+}
+
 // TestOrderUpdate is implemented by the application
 type OrderHandler func(ctx context.Context, event *models.TestOrderUpdateEvent) error
 
