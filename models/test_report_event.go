d654e7a1084145603d2ea2220449a8aa325c0669
Ashish Kheterpal <Ashish.Kheterpal@personalis.com>
2026-08-14 05:06:46 -0700

    Report Event

--- models/test_report_event.go
+++ models/test_report_event.go
@@ -3,6 +3,10 @@ package models
 import "time"
 
 type TestReportEvent struct {
+	EventVersion string    `json:"event_version"`
+	EventType    string    `json:"event_type"`
+	OccurredAt   time.Time `json:"occurred_at"`
+
 	TestOrderID          uint       `json:"TestOrderID"`
 	AccessionID          string     `json:"AccessionID"`
 	ReportDate           *time.Time `json:"ReportDate"`
@@ -17,4 +21,12 @@ type TestReportEvent struct {
 	FilePath             string     `json:"FilePath"`
 	MimeType             string     `json:"MimeType"`
 	CreatedBy            string     `json:"CreatedBy"`
+
+	Variants []Variant `json:"Variants"`
+}
+
+type Variant struct {
+	Mutation string  `json:"mutation"`
+	VAF      float32 `json:"vaf"`
+	Blurb    string  `json:"blurb"`
 }
